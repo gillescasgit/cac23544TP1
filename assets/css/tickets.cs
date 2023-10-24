12b207dce15d50409ea403fdb3fcd3edf08de0ae
Gonzalo Illescas <gillescas@jst.gob.ar>
2023-10-24 16:39:10 -0300

    ticket v1

---
+++ assets/css/tickets.cs
@@ -0,0 +1,192 @@
+.centrar {
+    margin-top: 2%;
+    text-align: center;
+    gap: 1%;
+    margin-right: 16%;
+    margin-left: 16%;
+}
+
+.estirar {
+    margin-bottom: 15%;
+    margin-top: 15%;
+}
+
+#borde1 {
+    border: 1px #047dfe solid;
+}
+
+#borde2 {
+    border: 1px #4bb7c8 solid;
+}
+
+#borde3 {
+    border: 1px #ffc10a solid;
+}
+
+.abajo1 {
+    margin-bottom: 2%;
+}
+
+.estirar {
+    padding-left: 25%;
+}
+
+#arriba1 {
+    margin-top: -1%;
+}
+
+#input_correo {
+    margin-bottom: 2%;
+}
+
+#cantidad {
+    margin-left: 0.5%;
+}
+
+#categoria {
+    margin-right: 0%;
+}
+
+#separar {
+    gap: 1%;
+    padding-left: 1.5%;
+    padding-right: 1%;
+}
+
+#resumen {
+    padding-left: 1%;
+}
+
+
+/*Para dar mismo espacio entre palabras a "Prensa"*/
+
+.izq {
+    margin-left: 10%;
+}
+
+
+/*MEDIA QUERY*/
+
+@media (max-width: 900px) {
+    .margen {
+        margin-left: -1%;
+    }
+    #mov-menu {
+        margin-left: 1%;
+    }
+    #card1 {
+        margin-top: -3%;
+        border-radius: inherit;
+    }
+    #t1 {
+        margin-left: 5%;
+        margin-right: 5%;
+    }
+    #sub {
+        position: relative;
+        top: -42px;
+        font-size: 38px;
+    }
+    #text-card {
+        display: contents;
+        position: relative;
+        margin-top: -85px;
+        text-align: left;
+        font-size: 19px;
+        margin-right: -1%;
+        margin-top: 1%;
+    }
+    #ajustar {
+        height: 206px;
+    }
+    #posicion {
+        position: relative;
+        top: -30px;
+        margin-left: 2%;
+        margin-right: 2%;
+    }
+    #botones-abajo {
+        position: relative;
+        top: -25px;
+        margin-left: 5%;
+        margin-right: 5%;
+    }
+    #separado {
+        margin-top: 15px;
+        text-align: center;
+    }
+    .buttons {
+        display: unset;
+    }
+    .bt1 {
+        text-align: center;
+        color: #353a40;
+        background: #fefdf3;
+        border: 1px solid #fefdf3;
+        border-radius: 9px;
+        padding: 5px 15px;
+        margin-top: 3%;
+    }
+    .bt2 {
+        border: 1px;
+        border-radius: 9px;
+        padding: 5px 5px;
+    }
+    .achico {
+        margin-left: 40%;
+        margin-right: 1%;
+    }
+    .bt10 {
+        color: #fefdf3;
+        background: transparent;
+        border: 1px solid;
+        border-radius: 4px;
+        padding: 5px 15px;
+    }
+    #text-abajo {
+        margin-top: 5%;
+    }
+    #text-01 {
+        margin-bottom: -4.5%;
+        font-size: 60%;
+    }
+    #abajo {
+        margin-top: 8%;
+        margin-left: 1%;
+    }
+    #text-abajo-01 {
+        margin-top: 10%;
+    }
+    #r1 {
+        display: flex;
+        flex-flow: column;
+    }
+    #form {
+        margin-left: 5%;
+        margin-right: 5%;
+    }
+    #td4 {
+        margin-bottom: 7%;
+    }
+    .footer {
+        display: none;
+    }
+    .miAcordion {
+        display: inline;
+    }
+    #color02 {
+        background: linear-gradient(to right, #1c3846, #1e4f6d);
+    }
+    /**********HTML tickets**********/
+    .centrar {
+        margin-left: 11%;
+    }
+    .abajo1 {
+        margin-bottom: 4%;
+    }
+    #separar {
+        gap: 3%;
+        padding-left: 0%;
+        padding-right: 1%;
+    }
+}
---
+++ assets/js/tickets.js
@@ -0,0 +1,69 @@
+function getValorbyID(id){
+    return document.getElementById(id).value;
+}
+
+
+
+function borrar(){
+    console.log('hallo, guten tag');
+}
+
+function calcularTotal(){
+    let id='res'
+    let total='-'
+    debugger
+
+    if(validarCantidad()){
+        let res =(200 * Number(getValorbyID('cantidad'))) - (200 * Number(getValorbyID('cantidad')) * obtenerDecuento());
+        total=res;
+    }
+
+    setValorbyID(id,total);    
+}
+
+
+function obtenerDecuento(){
+    var retorno=0;
+
+    let opcionValor = document.getElementById('opciones').value;
+
+    console.log(opcionValor );
+
+switch (opcionValor)
+    {
+        case "Estudiante":
+            retorno=0.15;
+            break;
+
+        case "Trainee":
+            retorno=0.50;
+            break;
+
+        case "Junior":
+            retorno=0.80;
+            break;
+        case "General":
+            retorno=0.0;
+            break;
+        default:
+            retorno=0.0;
+            break;
+
+        }
+    return retorno;
+}
+
+function validarCantidad(){
+
+    let cantidad=document.getElementById('cantidad').value;
+    debugger
+
+    if(Number(cantidad==0))
+        return false;
+
+    return true;
+
+}
+function setValorbyID(id,valor){
+    document.getElementById(id).innerHTML = valor;
+}
